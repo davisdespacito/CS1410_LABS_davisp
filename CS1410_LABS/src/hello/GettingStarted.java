57d2db6cc7c898ccdc7831871860b1f923c6a315
davisp_sequoia <davisp_sequoia@10.0.0.154>
2026-01-14 19:50:47 -0700

    First commit for CS1410_LABS java project.

---
+++ CS1410_LABS/src/hello/GettingStarted.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class GettingStarted {
+
+	public static void main(String[] args) {
+
+		String myName = "Davis";
+		System.out.println("Hi, I am " + myName + ".");
+		System.out.println("\"By changing nothing, nothing changes.\" --Tony Robbins ");
+	}
+
+}
