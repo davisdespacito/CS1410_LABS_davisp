622faa706c5efcac43ddd2d7c83f23cc0a416a44
davisp_sequoia <davisp_sequoia@144.35.96.156>
2026-01-27 15:20:06 -0700

    @ challenge 3

--- CS1410_LABS/src/textProcessing/LabStringBuilder.java
+++ CS1410_LABS/src/textProcessing/LabStringBuilder.java
@@ -15,7 +15,15 @@ public class LabStringBuilder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-				
+		StringBuilder sb = new StringBuilder("Bats live");
+		sb.setCharAt(0, 'r');
+		sb.append(" on ");
+		System.out.printf("Output: %s%n", sb);
+		
+		StringBuilder temp = new StringBuilder(sb);
+		sb.append(temp.reverse());
+		System.out.printf("New sb: %s%n", sb);
+		
 
 	}
 
