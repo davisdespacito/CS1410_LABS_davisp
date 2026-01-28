9783798dd40834f6e38f4433273b10596a935951
davisp_sequoia <davisp_sequoia@localhost>
2026-01-27 20:38:13 -0700

    LabStringBuilder assignment finished 8:38pm

--- CS1410_LABS/src/textProcessing/LabStringBuilder.java
+++ CS1410_LABS/src/textProcessing/LabStringBuilder.java
@@ -1,29 +1,46 @@
 package textProcessing;
 
-
-/** 
- * CS1410
- * Davis Pessetto
- * 01/27/2026
+/**
+ * CS1410 Davis Pessetto 01/27/2026
  * 
  * 
  * 
- * */
+ */
 
 public class LabStringBuilder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 		StringBuilder sb = new StringBuilder("Bats live");
 		sb.setCharAt(0, 'r');
 		sb.append(" on ");
-		System.out.printf("Output: %s%n", sb);
-		
-		StringBuilder temp = new StringBuilder(sb);
-		sb.append(temp.reverse());
-		System.out.printf("New sb: %s%n", sb);
-		
+
+		StringBuilder tempReverse = new StringBuilder(sb.toString());
+		sb.append(tempReverse.reverse());
+		System.out.println(sb);
+
+		int doubleSpaceIndex = sb.indexOf("  ");
+		sb.deleteCharAt(doubleSpaceIndex);
+		sb.append('.').setCharAt(0, 'C');
+		System.out.println(sb);
+
+		sb.insert(0, '.');
+		StringBuilder sbFinal = new StringBuilder(sb.toString().toLowerCase());
+		for (int i = 0; i < sbFinal.length(); i++) {
+
+			if (sbFinal.charAt(i) == ' ') {
+
+				sbFinal.deleteCharAt(i);
+				i--;
+
+			}
+
+		}
+
+		StringBuilder reversed = new StringBuilder(sbFinal).reverse();
+		boolean isPalindrome = sbFinal.toString().equals(reversed.toString());
+				
+		System.out.println("sb: " + sbFinal + " isPalindrome: " + isPalindrome);
 
 	}
 
