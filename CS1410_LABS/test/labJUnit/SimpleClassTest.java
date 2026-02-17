d5f4eedd633c6e3bbe684eaf15fe14698e2d46a5
davisdespacito <ddpessetto10@gmail.com>
2026-02-16 20:18:41 -0700

    created SimpleClassTest jUnit test file; 8:18pm

---
+++ CS1410_LABS/test/labJUnit/SimpleClassTest.java
@@ -0,0 +1,14 @@
+package labJUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleClassTest {
+
+	@Test
+	void testIsLeapYear() {
+		fail("Not yet implemented");
+	}
+
+}
