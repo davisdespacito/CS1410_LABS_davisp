511fff15e9cc73aa97a59b07ae16ecebff10371b
davisdespacito <ddpessetto10@gmail.com>
2026-02-16 20:16:54 -0700

    created SimpleClass class inside of new package "labJUnit;" 8:16pm

---
+++ CS1410_LABS/src/labJUnit/SimpleClass.java
@@ -0,0 +1,20 @@
+package labJUnit;
+
+/**
+ * Simple class to demonstrate automated testing with JUnit Jupiter (JUnit 5).
+ *
+ * @author davisPessetto
+ */
+public class SimpleClass {
+
+   /**
+    * Determines whether a given year is a leap year.
+    *
+    * @param year the year to evaluate
+    * @return true if year is a leap year and false otherwise.
+    * @throws IllegalArgumentException if year is not positive. 
+    */
+    public static boolean isLeapYear(int year) {
+       return false; // TODO
+    }
+}
