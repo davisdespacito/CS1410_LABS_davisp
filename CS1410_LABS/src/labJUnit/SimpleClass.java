0e2d3a08b4d674ebf0236bdb1e612f15f5adb692
davisdespacito <ddpessetto10@gmail.com>
2026-02-16 20:49:24 -0700

    all tests passed after tweaking isLeapYear method; 8:49pm

--- CS1410_LABS/src/labJUnit/SimpleClass.java
+++ CS1410_LABS/src/labJUnit/SimpleClass.java
@@ -7,18 +7,22 @@ package labJUnit;
  */
 public class SimpleClass {
 
-   /**
-    * Determines whether a given year is a leap year.
-    *
-    * @param year the year to evaluate
-    * @return true if year is a leap year and false otherwise.
-    * @throws IllegalArgumentException if year is not positive. 
-    */
-    public static boolean isLeapYear(int year) {
-    	
-    	if (year < 0)
-    		throw new IllegalArgumentException("Year must be positive.");
-    	
-       return false; // TODO
-    }
+	/**
+	 * Determines whether a given year is a leap year.
+	 *
+	 * @param year the year to evaluate
+	 * @return true if year is a leap year and false otherwise.
+	 * @throws IllegalArgumentException if year is not positive.
+	 */
+	public static boolean isLeapYear(int year) {
+
+		if (year <= 0)
+			throw new IllegalArgumentException("Year must be positive.");
+		
+		if (year % 100 == 0 && year % 400 != 0)
+			return false;
+		
+		return year % 4 == 0;
+
+	}
 }
