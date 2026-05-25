3ca728906c6bbfc0ff50c93e963a86406502febf
davisdespacito <ddpessetto10@gmail.com>
2026-05-24 22:27:28 -0600

    began docComments project 22:27

---
+++ CS1410_LABS/src/docComments/Car.java
@@ -0,0 +1,25 @@
+package docComments;
+
+public class Car {
+
+	private String make;
+	private String model;
+	private int mpg;
+	private Car car;
+	
+	public Car(String make, String model, int mpg) {
+		//TODO
+	}
+	
+	public String getMake() {
+		return make;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public int getMpg() {
+		return mpg;
+	}
+}
---
+++ CS1410_LABS/src/docComments/TripPlanner.java
@@ -0,0 +1,39 @@
+package docComments;
+
+/**
+ * 
+ */
+public class TripPlanner {
+	
+	private String departure;
+	private String arrival;
+	private int distance;
+	
+	/**
+	 * 
+	 * @param departure
+	 * @param arrival
+	 * @param distance
+	 * @param car
+	 */
+	public TripPlanner(String departure, String arrival, int distance, Car car) {
+		
+		//TODO
+	
+	}
+	
+	public double fuelConsumption() {
+		//TODO
+		return 0;
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public String toString() {
+		//TODO
+		return null;
+	}
+
+}
